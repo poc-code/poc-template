cc6452a80af37929b6030c63b5f9c0a834e83574
uirapeixoto <uira.peixoto@gmail.com>
2021-09-07 19:40:27 -0300

    chore(testes_unitarios) : testes

---
+++ test/Poc-Template-Unit-Test/Model/EnderecoModelTest.cs
@@ -0,0 +1,9 @@
+﻿using Poc_Template_Core_Test.Helper;
+using Poc_Template_Domain.Entities;
+
+namespace Poc_Template_Unit_Test.Model
+{
+    public class EnderecoModelTest : ModelTest<Endereco>
+    {
+    }
+}
